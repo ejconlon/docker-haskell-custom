b1aa2ec33983682648cfc285de7d0e25def67952
Eric Conlon <ejconlon@gmail.com>
2021-09-17 11:25:57 -0700

    default tags

--- script/build.sh
+++ script/build.sh
@@ -8,7 +8,8 @@ shift
 NAMESPACE="${NAMESPACE:-ejconlon}"
 CABAL_INSTALL="3.6.0.0"
 STACK="2.7.3"
-TAG="${NAMESPACE}/haskell-custom:${GHC}-${ARCH}"
+SHORT_TAG="${NAMESPACE}/haskell-custom:${GHC}"
+LONG_TAG="${SHORT_TAG}-${ARCH}"
 
 if [ "${ARCH}" != "aarch64" ] && [ "${ARCH}" != "x86_64" ]; then
   echo "Need ARCH (aarch64/x86_64)"
@@ -16,5 +17,10 @@ if [ "${ARCH}" != "aarch64" ] && [ "${ARCH}" != "x86_64" ]; then
 fi
 
 pushd "images/common"
-  docker build -t ${TAG} --build-arg ARCH=${ARCH} --build-arg GHC=${GHC} --build-arg CABAL_INSTALL=${CABAL_INSTALL} --build-arg STACK=${STACK} .
+  docker build -t ${LONG_TAG} --build-arg ARCH=${ARCH} --build-arg GHC=${GHC} --build-arg CABAL_INSTALL=${CABAL_INSTALL} --build-arg STACK=${STACK} .
 popd
+
+# Default to x86_64 if arch is omitted from tag
+if [ "${ARCH}" == "x86_64" ]; then
+  docker tag ${LONG_TAG} ${SHORT_TAG}
+fi
--- script/push.sh
+++ script/push.sh
@@ -8,3 +8,8 @@ shift
 NAMESPACE="${NAMESPACE:-ejconlon}"
 
 docker push ${NAMESPACE}/haskell-custom:${GHC}-${ARCH}
+
+# See default tag logic in build.sh
+if [ "${ARCH}" == "x86_64" ]; then
+  docker push ${NAMESPACE}/haskell-custom:${GHC}
+fi
